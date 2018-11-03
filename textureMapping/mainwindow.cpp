c174d8d2d71be39fea7e2deb1defb5cbd878ae40
xiaozhuo1314 <chenzhuo1116@qq.com>
2018-11-03 22:37:42 +0800

    texturemapper

--- textureMapping/mainwindow.cpp
+++ textureMapping/mainwindow.cpp
@@ -3,9 +3,62 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    textureMappingCylinder();
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void MainWindow::textureMappingCylinder()
+{
+    //source
+    //读取jpeg的二维图
+    vtkSmartPointer<vtkJPEGReader> reader = vtkSmartPointer<vtkJPEGReader>::New();
+    reader->SetFileName("/home/silence/Project/VTK-Qt/textureMapping/texture.jpg");
+
+    //定义到纹理图
+    vtkSmartPointer<vtkTexture> texture = vtkSmartPointer<vtkTexture>::New();
+    texture->SetInputConnection(reader->GetOutputPort());
+    texture->InterpolateOn();  //貌似注释后没什么变化
+
+    //vtkSmartPointer<vtkPlaneSource> plane = vtkSmartPointer<vtkPlaneSource>::New(); //生成平面去被贴
+    //cylinder
+    vtkSmartPointer<vtkCylinderSource> cylinder = vtkSmartPointer<vtkCylinderSource>::New();
+    cylinder->SetHeight(3.0);
+    cylinder->SetRadius(1.0);
+    cylinder->SetResolution(10);
+
+    //mapper
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(cylinder->GetOutputPort());
+
+    //actor
+    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+    actor->SetTexture(texture);
+
+    //render
+    vtkSmartPointer<vtkRenderer> render = vtkSmartPointer<vtkRenderer>::New();
+    render->AddActor(actor);
+    render->SetBackground(0, 0, 0);
+
+    //renderwindow
+    vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
+    renWin->AddRenderer(render);
+    renWin->SetSize(640, 480);
+    renWin->Render();
+    renWin->SetWindowName("texture");
+
+    //renderwindowinteractor
+    vtkSmartPointer<vtkRenderWindowInteractor> interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    interactor->SetRenderWindow(renWin);
+
+    //vtkInteractorStyleTrackballCamera
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera> style = vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+    interactor->SetInteractorStyle(style);
+
+    interactor->Initialize();
+    interactor->Start();
 }
--- textureMapping/mainwindow.h
+++ textureMapping/mainwindow.h
@@ -2,6 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vtkJPEGReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkTexture.h>
+//#include <vtkPlaneSource.h>
+#include <vtkCylinderSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +23,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+public:
+    void textureMappingCylinder();
 };
 
 #endif // MAINWINDOW_H
